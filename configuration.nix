9d8ea1d794f73a6378e6444015cd6868334e0052
Rajdeep Singh <radewala.2@gmail.com>
2024-06-03 21:19:54 +0530

    fix the autologin error

--- configuration.nix
+++ configuration.nix
@@ -122,9 +122,8 @@
   };
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "officialrajdeepsingh";
-
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "officialrajdeepsingh";
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
