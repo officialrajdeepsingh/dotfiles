ca544954d7c4ebc8fa4eb01151b53c8e4dc27a6f
Rajdeep Singh <radewala.2@gmail.com>
2024-06-08 23:47:52 +0530

    format the code and remove the unused code

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,3 @@
-# Edit this configuration file to define what should be installed on your system.  Help is available in the configuration.nix(5) man page and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -81,7 +79,6 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     # jack.enable = true;
-
     # use the example session manager (no others are packaged yet so this is enabled by default, no need to redefine it in your config for now)
     # media-session.enable = true;
   };
