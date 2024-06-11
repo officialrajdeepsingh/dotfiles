e0e85176be03c029eb3f7c610f695944ccd9fcab
Rajdeep Singh <radewala.2@gmail.com>
2024-06-11 14:14:09 +0530

    format the file

--- home.nix
+++ home.nix
@@ -1,13 +1,18 @@
-{config, pkgs, inputs, ... }:
 {
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  # programs.nixvim.enable = true;
   # Home Manager needs a bit of information about you and the paths it should manage.
-  
+
   home.username = "officialrajdeepsingh";
   home.homeDirectory = "/home/officialrajdeepsingh";
 
   # Enable https://devenv.sh
   # This value determines the Home Manager release that your configuration is compatible with. This helps avoid breakage when a new Home Manager release introduces backwards incompatible changes. You should not change this value, even if you update Home Manager. If you do want to update the value, then make sure to first check the Home Manager release notes.
- 
+
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
   # command for install or rebuild system :  sudo nixos-rebuild switch --flake /etc/nixos/#default
@@ -19,108 +24,99 @@
   home.enableNixpkgsReleaseCheck = false;
 
   home.packages = with pkgs; [
-  
-  # Database-cli (install for https://www.openstatus.dev )
+    # Database-cli (install for https://www.openstatus.dev )
     turso-cli
 
-  # Browser
+    # Browser
     google-chrome
     tor-browser
     brave
-  
-  # Notes App
+
+    # Notes App
     notesnook
 
-  # Language
+    # Language
     rustup
     lua
     zig
     python3
 
-  # Install for meshery UI
+    # Install for meshery UI
     # go
     # cope
     # gnumake
     # gnumake42
 
-      
-  # IDE
-    lunarvim
-     
-  # JavaScript
+    # IDE
+    # lunarvim
+
+    # JavaScript
     nodejs_20
     # nodejs_18
     deno
     # nodePackages_latest.npm
     nodePackages_latest.yarn
-    nodePackages_latest.pnpm     
+    nodePackages_latest.pnpm
     # turbo
     node2nix
 
-  # Terminal 
+    # Terminal
     lazygit
     bottom
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+    (nerdfonts.override {fonts = ["FiraCode"];})
 
-  # Utility
+    # Utility
     git
     gh
     act
     unzip
     curl
-    unzip 
+    unzip
     wget
 
-  # Utility Command
+    # Utility Command
     xclip
     tree
     hugo
-    
-  # thunderbird
 
-  # podcasts
+    # thunderbird
+
+    # podcasts
     # gnome-podcasts
 
-  # office
+    # office
     # libreoffice
-   
-  # database
+
+    # database
     # surrealdb
     # nodePackages.prisma
 
-
-  # others
+    # others
     # upwork
     zoom-us
     openssl
-   
-    
+
     # It is sometimes useful to fine-tune packages, for example, by applying overrides. You can do that directly here, just don't forget the parentheses. Maybe you want to install Nerd Fonts with a limited number of fonts?
 
     # You can also create simple shell scripts directly inside your configuration. For example, this adds a command 'my-hello' to your environment:
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-
   ];
 
-  
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage plain files is through 'home.file'.
-  
-  home.file = {
 
+  home.file = {
     # Building this configuration will create a copy of 'dotfiles/screenrc' in the Nix store. Activating the configuration will then make '~/.screenrc' a symlink to the Nix store copy.
-    
+
     # ".screenrc".source = dotfiles/screenrc;
 
     # You can also set the file content immediately.
-    
+
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  
   };
 
   # Home Manager can also manage your environment variables through 'home.sessionVariables'. If you don't want to manage your shell through Home Manager then you have to manually source 'hm-session-vars.sh' located at  either.
@@ -131,11 +127,10 @@
   # or
   #  /etc/profiles/per-user/officialrajdeepsingh/etc/profile.d/hm-session-vars.sh
 
-   home.sessionVariables = {
-     EDITOR = "nvim";
+  home.sessionVariables = {
+    EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
 }
