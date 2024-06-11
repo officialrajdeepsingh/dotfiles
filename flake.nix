e86e766d7871d93a9b756bafb14093d884f91959
Rajdeep Singh <radewala.2@gmail.com>
2024-06-11 14:13:56 +0530

    install nixvim

--- flake.nix
+++ flake.nix
@@ -1,29 +1,21 @@
 {
   description = "Nixos config flake";
 
-   inputs = {
-  
+  inputs = {
     ## NixPkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
+
     ## Home Manager
     home-manager = {
-       url = "github:nix-community/home-manager";
-       inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ## Nixvim
-    # nixvim = {
-    #  url = "github:nix-community/nixvim";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    ## NixVim
+    nixvim = {
+      url = "github:elythh/nixvim";
+    };
 
-    nixvim.url = "github:officialrajdeepsingh/nixvim/custom";
-
-    # nixvim.url = "path:./nixvim";
-     
-    # nixvim.url = "git+file:./nixvim/flake.nix";
-    
     # ---Auto CPU---
     # auto-cpufreq = {
     #  url = "github:AdnanHodzic/auto-cpufreq";
@@ -32,27 +24,31 @@
     # ---Auto CPU---
   };
 
- outputs = { self, nixpkgs, home-manager, nixvim, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {  
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [ 
-            ./configuration.nix
-          ];
-      };
-      
-      homeConfigurations.default = home-manager.lib.homeManagerConfiguration {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        inherit pkgs;
-        modules = [
-          ./home.nix
-        ];
-        extraSpecialArgs = { inherit inputs; };
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nixvim,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+      ];
+    };
+
+    homeConfigurations.default = home-manager.lib.homeManagerConfiguration {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      inherit pkgs;
+      modules = [
+        ./home.nix
+      ];
+      extraSpecialArgs = {inherit inputs;};
+    };
   };
 }
