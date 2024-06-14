1f5d7819c8fb2c3b1fff4384b3955738918e745a
Rajdeep Singh <radewala.2@gmail.com>
2024-06-14 10:53:20 +0530

    enable nixvim with elythh flake

--- configuration.nix
+++ configuration.nix
@@ -1,19 +1,16 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, ... }: {
 
-{
-
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./services
-      ./programs
-    ];
-
-  # trusted users config for https://devenv.sh/getting-started/
-  nix.settings.trusted-users = [ "root" "officialrajdeepsingh" ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./services
+    ./programs
+  ];
+  
+  # trusted users config for https://devenv.sh/getting-started
+  nix.settings.trusted-users = ["root" "officialrajdeepsingh"];
 
   # Bootloader.
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -56,8 +53,8 @@
   services.xserver.desktopManager.gnome.enable = true;
 
   # remove preinstall or  unused package in gnome
-  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome.gnome-music nixos-render-docs  ];
-  services.xserver.excludePackages = with  pkgs; [ xterm ];
+  environment.gnome.excludePackages = with pkgs; [gnome-tour gnome.gnome-music nixos-render-docs];
+  services.xserver.excludePackages = with pkgs; [xterm];
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -77,54 +74,53 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    
     # If you want to use JACK applications, uncomment this
     # jack.enable = true;
     # use the example session manager (no others are packaged yet so this is enabled by default, no need to redefine it in your config for now)
     # media-session.enable = true;
+    
   };
 
   # Enable experimental-features in nixos
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # change the deafult bash Shell to zsh shell
   users.defaultUserShell = pkgs.zsh;
-  environment.shells = with pkgs; [ zsh ];
+  environment.shells = with pkgs; [zsh];
 
   # Define env Editor for sudo
-  environment.variables= {
+  environment.variables = {
     EDITOR = "lvim";
     VISUAL = "lvim";
     SUDO_EDITOR = "lvim";
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  
+
   users.users.officialrajdeepsingh = {
     isNormalUser = true;
     description = "officialrajdeepsingh";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = ["networkmanager" "wheel" "docker"];
     # openssh.authorizedKeys.keyFiles = [  .ssh/id_ed25519.pub ];
     packages = with pkgs; [
-  
-      ## github:elythh/nixvim 
-      inputs.nixvim.packages.${pkgs.system}.default
-
-      # Browser
-      # google-chrome
-      # vscode
+    
+      ## github:elythh/nixvim
+       inputs.nixvim.packages.${pkgs.system}.default
     ];
   };
 
   # docker Config
   virtualisation.docker.enable = true;
   virtualisation.docker.rootless = {
-     enable = true;
-     setSocketVariable = true;
+    enable = true;
+    setSocketVariable = true;
   };
 
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "officialrajdeepsingh";
+  
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
@@ -132,8 +128,7 @@
   # Allow unfree packages
   # nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile. To search, run: $ nix search wget
   # environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
@@ -152,10 +147,8 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default settings for stateful data, like file locations and database versions on your system were taken. It‘s perfectly fine and recommended to leave
+  # This value determines the NixOS release from which the default settings for stateful data, like file locations and database versions on your system were taken. It‘s perfectly fine and recommended to leave.
   # this value at the release version of the first install of this system. Before changing this value read the documentation for this option. (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  
+
   system.stateVersion = "23.05"; # Did you read the comment?
-
 }
-
