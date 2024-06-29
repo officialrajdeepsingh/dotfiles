d2bc12c9f5b5a21e2cb0b0bd502565aae881e220
Rajdeep Singh <radewala.2@gmail.com>
2024-06-29 14:16:32 +0530

    remove the unused argument

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -1,28 +1,27 @@
-{config, pkgs, inputs,  ... }:
 {
-    programs = {
-      zsh = {
-        enable = true;
-        autosuggestions.enable = true;
-        zsh-autoenv.enable = true;
-        syntaxHighlighting.enable = true;
-        shellAliases = {
-          home = "home-manager switch -f ~/nixos-config/home.nix";
-          build = "sudo nixos-rebuild switch  --flake ~/nixos-config/#default";
-          build-test = "sudo nixos-rebuild test --flake ~/nixos-config/#default";
-        };
-        ohMyZsh   =  {
-          enable  =  true;
-          theme   =  "robbyrussell";
-          plugins = [
-            "git"
-            "npm"
-            "history"
-            "node"
-            "rust"
-            "deno"
-          ];
-        };
+  programs = {
+    zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      zsh-autoenv.enable = true;
+      syntaxHighlighting.enable = true;
+      shellAliases = {
+        home = "home-manager switch -f ~/nixos-config/home.nix";
+        build = "sudo nixos-rebuild switch  --flake ~/nixos-config/#default";
+        build-test = "sudo nixos-rebuild test --flake ~/nixos-config/#default";
       };
+      ohMyZsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = [
+          "git"
+          "npm"
+          "history"
+          "node"
+          "rust"
+          "deno"
+        ];
+      };
+    };
   };
 }
