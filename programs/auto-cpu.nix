745e38e2054eaef3b21cac78f28ed2d30d454496
Rajdeep Singh <radewala.2@gmail.com>
2024-10-09 15:46:58 +0530

    uncomment unused nix code

--- programs/auto-cpu.nix
+++ programs/auto-cpu.nix
@@ -7,21 +7,4 @@
 
    services.auto-cpufreq.enable = true;
 
-   # --- Flake code unused  ---
-  
-   # programs.auto-cpufreq.enable = true;
-    
-   # Optionally, you can configure your auto-cpufreq settings, if you have any
-   
-   #   programs.auto-cpufreq.settings = {
-   #   charger = {
-   #     governor = "performance";
-   #     turbo = "auto";
-   #   };
-
-   #   battery = {
-   #     governor = "powersave";
-   #     turbo = "auto";
-   #   };
-   # };
 }
