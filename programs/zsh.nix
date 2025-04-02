3645dc44595f94236b345b2574e7392107e6153b
Rajdeep Singh <radewala.2@gmail.com>
2025-04-02 15:22:05 +0530

    format the file and remove the --flake option in home aliases

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -5,13 +5,13 @@
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
       shellAliases = {
-        home = "home-manager switch --flake ~/dotfiles/home.nix";
+        home = "home-manager switch -f ~/dotfiles/home.nix";
         build = "sudo nixos-rebuild switch  --flake ~/dotfiles/#default";
         build-test = "sudo nixos-rebuild test --flake ~/dotfiles/#default";
         upgrade = "sudo nixos-rebuild switch --upgrade  --flake ~/dotfiles/#default";
         rm-home = "home-manager  remove-generations";
         li-home = "home-manager  generations";
-        rm-nixos = "sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations"; 
+        rm-nixos = "sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations";
         li-nixos = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
       };
       initExtra = ''
