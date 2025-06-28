778c69972d159759f29e13e64dbea50decf445f4
Rajdeep Singh <radewala.2@gmail.com>
2025-06-28 11:42:36 +0530

    fix the file format

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -5,21 +5,21 @@
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
       shellAliases = {
-        home = "home-manager switch -f ~/dotfiles/home.nix";
-        build = "sudo nixos-rebuild switch  --flake ~/dotfiles/#default";
+        home   =  "home-manager switch -f ~/dotfiles/home.nix";
+        build  =  "sudo nixos-rebuild switch  --flake ~/dotfiles/#default";
         build-test = "sudo nixos-rebuild test --flake ~/dotfiles/#default";
-        upgrade = "sudo nixos-rebuild switch --upgrade  --flake ~/dotfiles/#default";
-        rm-home = "home-manager  remove-generations";
-        li-home = "home-manager  generations";
+        upgrade  = "sudo nixos-rebuild switch --upgrade  --flake ~/dotfiles/#default";
+        rm-home  = "home-manager  remove-generations";
+        li-home  = "home-manager  generations";
         rm-nixos = "sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations";
         li-nixos = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
       };
-      initExtra = ''
+      initContent = ''
 # PNPM HOME Env
 export PNPM_HOME="/home/officialrajdeepsingh/.local/share/pnpm"
 case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
+  *":$PNPM_HOME:"*);
+  *) export PATH="$PNPM_HOME:$PATH";
 esac
 
 # FNM Manage the Node version
