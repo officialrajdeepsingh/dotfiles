d9e35c70bb94627d8967277f69aafc26bf9c4f74
Rajdeep Singh <radewala.2@gmail.com>
2025-06-28 13:21:54 +0530

    rename the services.xserver.displayManager.gdm.enable and services.xserver.displayManager.gnome.enable

--- configuration.nix
+++ configuration.nix
@@ -55,8 +55,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # remove preinstall or  unused package in gnome
   environment.gnome.excludePackages = with pkgs; [
