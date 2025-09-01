5c109a508b5e0246640c2fa032d7ff5c375775e7
Rajdeep Singh <radewala.2@gmail.com>
2025-09-01 18:43:43 +0530

    remove the nixvim

--- flake.nix
+++ flake.nix
@@ -20,6 +20,11 @@
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
 
+    # nixvim = {
+    #   url = "github:nix-community/nixvim";
+    #   nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    #
   };
 
   outputs =
