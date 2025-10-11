07fb8d48a5785e1cea4210e7af47d090f3cfe995
Rajdeep Singh <radewala.2@gmail.com>
2025-10-11 17:52:41 +0530

    config the shellAliases

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -5,26 +5,26 @@
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
       shellAliases = {
-        home   =  "home-manager switch -f ~/dotfiles/home.nix";
-        build  =  "sudo nixos-rebuild switch  --flake ~/dotfiles/#default";
+        home = "home-manager switch --flake ~/dotfiles#officialrajdeepsingh@nixos";
+        build = "sudo nixos-rebuild switch  --flake ~/dotfiles/#default";
         build-test = "sudo nixos-rebuild test --flake ~/dotfiles/#default";
-        upgrade  = "sudo nixos-rebuild switch --upgrade  --flake ~/dotfiles/#default";
-        rm-home  = "home-manager  remove-generations";
-        li-home  = "home-manager  generations";
+        upgrade = "sudo nixos-rebuild switch --upgrade  --flake ~/dotfiles/#default";
+        rm-home = "home-manager  remove-generations";
+        li-home = "home-manager  generations";
         rm-nixos = "sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations";
         li-nixos = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
       };
       initContent = ''
-# PNPM HOME Env
-export PNPM_HOME="/home/officialrajdeepsingh/.local/share/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*)  ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
+        # PNPM HOME Env
+        export PNPM_HOME="/home/officialrajdeepsingh/.local/share/pnpm"
+        case ":$PATH:" in
+          *":$PNPM_HOME:"*)  ;;
+          *) export PATH="$PNPM_HOME:$PATH" ;;
+        esac
 
-# FNM Manage the Node version
-eval "$(fnm env --use-on-cd --shell zsh)"
-'';
+        # FNM Manage the Node version
+        eval "$(fnm env --use-on-cd --shell zsh)"
+      '';
       completionInit = "pnpm setup";
       oh-my-zsh = {
         enable = true;
